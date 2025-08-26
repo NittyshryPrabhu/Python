e2f98c5e20183fb43583ed9ee428cace30ae065e
Nittyshry Prabhu <nittyshryprabh@gmail.com>
2025-08-26 22:00:48 +0530

    Added 2 Program in this file

--- day4.py
+++ day4.py
@@ -1,2 +1,21 @@
 # Loops (for, while, break, continue)
 
+# 📝 Practice: Loops in Python
+
+
+#🔹 Beginner Exercises
+
+# 1. Print numbers from 1 to 10 using a for loop.
+
+'''for i in range(1,11):
+    print(i)'''
+   
+# 2. Print numbers from 10 down to 1 using a while loop.
+
+'''i=10
+while i>=1:
+    print(i)
+    i -= 1'''
+# Print the multiplication table of 5 using a for loop.
+# Print each character of the string "PYTHON" using a for loop.
+# Print only even numbers from 1 to 20 using a for loop.
